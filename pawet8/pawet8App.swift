2ca82078a62932b059029f6ae621878e1600a95c
farah alshammari <farah.ntuf@outlook.sa>
2023-10-08 09:33:54 +0300

    Initial Commit

---
+++ pawet8/pawet8App.swift
@@ -0,0 +1,17 @@
+//
+//  pawet8App.swift
+//  pawet8
+//
+//  Created by Farah mohammed alshammari on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct pawet8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
